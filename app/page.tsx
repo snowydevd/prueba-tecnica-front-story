c390793224e69dd0a5b87dc50454e5fe25f0c3cd
snowydevd <lautarochini4@gmail.com>
2026-06-13 14:07:15 -0300

    font fix

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,11 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <section>
+      <div className="min-w-2xl flex justify-between items-center text-gray-700 ">
+        <span className="font-sans leading-6">Lautaro Chini</span>
+        <span>Front Story</span>
+      </div>
+      <section className="shadow-2xl p-16 rounded-lg">
         <div className="flex justify-between items-center min-w-2xl mb-10">
           <h1 className="text-2xl font-bold">Campaigns</h1>
           <AddCampaignDialog onAdd={refresh} />
